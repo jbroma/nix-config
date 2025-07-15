bc54815e40914ae3bbe4c672d99c42847b2cad2f
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-15 21:56:17 +0200

    bat: done

--- home-manager/fzf.nix
+++ home-manager/fzf.nix
@@ -11,9 +11,9 @@ in {
     defaultCommand = "${fd} -H --type f";
     defaultOptions = ["--height 50%"];
     fileWidgetCommand = "${defaultCommand}";
-    # fileWidgetOptions = [
-    #   "--preview '${lib.getExe pkgs.bat} --color=always --plain --line-range=:200 {}'"
-    # ];
+    fileWidgetOptions = [
+      "--preview '${lib.getExe pkgs.bat} --color=always --plain --line-range=:200 {}'"
+    ];
     changeDirWidgetCommand = "${fd} -H --type d";
     changeDirWidgetOptions = ["--preview '${pkgs.tree}/bin/tree -C {} | head -200'"];
     historyWidgetOptions = [];
