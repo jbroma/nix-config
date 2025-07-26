99073b5c8b4dd0c1867b6a34acfdf7154991d73d
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-26 16:28:57 +0200

    macos: drop control center order

--- macos/control-center.nix
+++ macos/control-center.nix
@@ -1,16 +1,5 @@
-{ 
-  lib, 
-  ...
-}:
+{ ... }:
 
-let
-  # Helper function to generate NSStatusItem visibility attributes
-  nsStatusItemVisible = items:
-    lib.attrsets.mapAttrs' (name: value: {
-      name = "NSStatusItem Visible ${name}";
-      value = value;
-    }) items;
-in
 {
   # menu bar clock settings
   system.defaults.menuExtraClock = {
@@ -25,14 +14,8 @@ in
   };
 
   system.defaults.CustomUserPreferences = {
-    "com.apple.controlcenter" = nsStatusItemVisible {
-      Battery = 0;
-      BentoBox = 1;
-      Clock = 1;
-      FocusModes = 1;
-      NowPlaying = 0;
-      Sound = 1;
-      WiFi = 1;
+    "com.apple.controlcenter" = {
+      BatteryShowPercentage = false;
     };
 
     "com.apple.Spotlight" = {
