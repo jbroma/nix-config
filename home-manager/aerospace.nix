e55833939a0084afcba80328646ac059db38b943
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-27 21:16:43 +0200

    aerospace: customized config

--- home-manager/aerospace.nix
+++ home-manager/aerospace.nix
@@ -63,6 +63,32 @@
           "if".app-name-regex-substring = "emulator";
           run = "layout floating";
         }
+        # App Automatic Workspace Assignment
+        {
+          "if".app-id = "com.mitchellh.ghostty";
+          run = "move-node-to-workspace 1";
+        }
+        {
+          "if".app-id = "com.google.Chrome";
+          run = "move-node-to-workspace 2";
+        }
+        {
+          # Cursor
+          "if".app-id = "com.todesktop.230313mzl4w4u92";
+          run = "move-node-to-workspace 3";
+        }
+        {
+          "if".app-id = "com.tinyspeck.slackmacgap";
+          run = "move-node-to-workspace 4";
+        }
+        {
+          "if".app-id = "com.hnc.Discord";
+          run = "move-node-to-workspace 4";
+        }
+        {
+          "if".app-id = "com.spotify.client";
+          run = "move-node-to-workspace 5";
+        }
       ];
 
       # Possible values: (qwerty|dvorak)
@@ -80,14 +106,14 @@
       #                 See: https://nikitabobko.github.io/AeroSpace/guide#assign-workspaces-to-monitors
       gaps = {
         inner = {
-          horizontal = 10;
-          vertical = 10;
+          horizontal = 16;
+          vertical = 16;
         };
         outer = {
-          left = 10;
-          bottom = 10;
-          top = 10;
-          right = 10;
+          left = 16;
+          bottom = 16;
+          top = 56;
+          right = 16;
         };
       };
 
@@ -126,16 +152,16 @@
           alt-comma = "layout accordion horizontal vertical";
 
           # See: https://nikitabobko.github.io/AeroSpace/commands#focus
-          alt-h = "focus left";
-          alt-j = "focus down";
-          alt-k = "focus up";
-          alt-l = "focus right";
+          alt-left = "focus left";
+          alt-down = "focus down";
+          alt-up = "focus up";
+          alt-right = "focus right";
 
           # See: https://nikitabobko.github.io/AeroSpace/commands#move
-          alt-shift-h = "move left";
-          alt-shift-j = "move down";
-          alt-shift-k = "move up";
-          alt-shift-l = "move right";
+          alt-shift-left = "move left";
+          alt-shift-down = "move down";
+          alt-shift-up = "move up";
+          alt-shift-right = "move right";
 
           # See: https://nikitabobko.github.io/AeroSpace/commands#resize
           alt-shift-minus = "resize smart -50";
@@ -147,6 +173,10 @@
           alt-3 = "workspace 3";
           alt-4 = "workspace 4";
           alt-5 = "workspace 5";
+          alt-6 = "workspace 6";
+          alt-7 = "workspace 7";
+          alt-8 = "workspace 8";
+          alt-9 = "workspace 9";
 
           # See: https://nikitabobko.github.io/AeroSpace/commands#move-node-to-workspace
           alt-shift-1 = "move-node-to-workspace 1";
@@ -154,6 +184,10 @@
           alt-shift-3 = "move-node-to-workspace 3";
           alt-shift-4 = "move-node-to-workspace 4";
           alt-shift-5 = "move-node-to-workspace 5";
+          alt-shift-6 = "move-node-to-workspace 6";
+          alt-shift-7 = "move-node-to-workspace 7";
+          alt-shift-8 = "move-node-to-workspace 8";
+          alt-shift-9 = "move-node-to-workspace 9";
 
           # See: https://nikitabobko.github.io/AeroSpace/commands#workspace-back-and-forth
           alt-tab = "workspace-back-and-forth";
@@ -183,23 +217,19 @@
             "close-all-windows-but-current"
             "mode main"
           ];
-
-          # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
-          #s = [ "layout sticky tiling" "mode main" ];
-
-          alt-shift-h = [
+          alt-shift-left = [
             "join-with left"
             "mode main"
           ];
-          alt-shift-j = [
+          alt-shift-down = [
             "join-with down"
             "mode main"
           ];
-          alt-shift-k = [
+          alt-shift-up = [
             "join-with up"
             "mode main"
           ];
-          alt-shift-l = [
+          alt-shift-right = [
             "join-with right"
             "mode main"
           ];
