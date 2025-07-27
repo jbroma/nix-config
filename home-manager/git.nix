9f6f21af6a45b50cea53a197f96a4c9089e2323a
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-27 14:30:27 +0200

    git: add aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -12,6 +12,28 @@
     userName = "Jakub Romanczyk";
     userEmail = if type == "work" then "jakub.romanczyk@callstack.com" else "j.romanczyk@gmail.com";
     extraConfig = {
+      alias = {
+        s = "status";
+        l = "log";
+        d = "diff";
+        dc = "diff --cached";
+        a = "add";
+        c = "commit";
+        ca = "commit --amend";
+        cn = "commit --amend --no-edit";
+        ch = "checkout";
+        cp = "cherry-pick";
+        sw = "switch";
+        pushf = "push --force-with-lease";
+        publish = "!git push -u origin $(git rev-parse --abbrev-ref HEAD)";
+        r1 = "rebase HEAD^ -i";
+        r2 = "rebase HEAD~2 -i";
+        r3 = "rebase HEAD~3 -i";
+        r4 = "rebase HEAD~4 -i";
+        r5 = "rebase HEAD~5 -i";
+        dt = "difftool --tool=vimdiff -y";
+        dtc = "dt --cached -y";
+      };
       core.editor = "vim";
       init.defaultBranch = "main";
       merge.conflictstyle = "zdiff3";
