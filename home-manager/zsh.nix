d6ea37048e92fa335d2f3855b424b20a77be000b
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-27 14:30:57 +0200

    zsh: add plugins

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -6,8 +6,22 @@
 }:
 
 {
+  # install plugin packages
+  home.packages = with pkgs; [
+    zsh-fzf-tab
+    zsh-autosuggestions
+    zsh-fast-syntax-highlighting
+    zsh-you-should-use
+  ];
+
   programs.zsh = {
     enable = true;
+
+    localVariables = {
+      # enforce using aliases
+      YSU_HARDCORE = 1;
+    };
+
     shellAliases = {
       darwin-rebuild-switch = "sudo ~/.nix/rebuild-and-switch.sh";
       darwin-cleanup = "sudo nix-collect-garbage --delete-older-than 7d";
@@ -15,5 +29,30 @@
       cat = "bat";
       code = "cursor";
     };
+
+    plugins = [
+      {
+        # Must be before plugins zsh-autosuggestions && fast-syntax-highlighting
+        name = "zsh-fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.zsh-autosuggestions;
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+      }
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+      }
+      {
+        name = "zsh-you-should-use";
+        src = pkgs.zsh-you-should-use;
+        file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+      }
+    ];
+
   };
 }
