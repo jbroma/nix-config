0a9eee92278a2f7b08191c8c701f630ccb49bfe6
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-28 15:48:08 +0200

    sketchybar: fix aerospace plugin

--- dotfiles/sketchybar/plugins/aerospace.sh
+++ dotfiles/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+# Use FOCUSED_WORKSPACE if defined, otherwise check the focused workspace
+FW="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
+
+if [ "$1" = "$FW" ]; then
   sketchybar --set $NAME label.color=0xffffffff
 else
-  sketchybar --set $NAME label.color=0x779a9a9a
+  sketchybar --set $NAME label.color=0xff9a9a9a
 fi
