d665b91b8c3e7b9a891b73a5e991137133c5964b
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2026-01-15 11:55:24 +0100

    spotify: remove ovveride

--- flake.nix
+++ flake.nix
@@ -75,16 +75,6 @@
                   claude-island = customPkgs.claude-island;
                   cursor = pkgs.code-cursor;
                   cleanshot-x = customPkgs.cleanshot-x;
-                  spotify = super.spotify.overrideAttrs (oldAttrs: {
-                    src =
-                      if (super.stdenv.isDarwin && super.stdenv.isAarch64) then
-                        super.fetchurl {
-                          url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-                          hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-                        }
-                      else
-                        oldAttrs.src;
-                  });
                 })
                 inputs.nix-vscode-extensions.overlays.default
               ];
