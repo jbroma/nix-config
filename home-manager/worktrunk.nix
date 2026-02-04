e46b4177c52e432b5824f1a8e45a36920a826475
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2026-02-04 16:22:25 +0100

    worktrunk: add shell integration

--- home-manager/worktrunk.nix
+++ home-manager/worktrunk.nix
@@ -5,4 +5,9 @@
 {
   # User config: ~/.config/worktrunk/config.toml
   xdg.configFile."worktrunk/config.toml".source = ../dotfiles/worktrunk/config.toml;
+
+  # Shell integration
+  programs.zsh.initContent = ''
+    eval "$(wt config shell init zsh)"
+  '';
 }
